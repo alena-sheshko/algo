55343fcfdb82b2701fa328fc9f1631f8bca535be
alena-sheshko <alena-sheshko@users.noreply.github.com>
2016-03-06 14:52:38 +0300

    Added files via upload

--- next_permutation/solution.py
+++ next_permutation/solution.py
@@ -4,57 +4,52 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-
-        #find pivot
-        pivot = len(nums) - 1
-        i = pivot - 1
+        i = len(nums) - 2
         while i >= 0:
-            if nums[i] < nums[pivot]:
-                pivot = i
-                break
-            pivot = i
-            i -= 1
-
-        # replace pivot
-        if i >= 0:
-            if pivot == len(nums) - 2:
-                nums[pivot], nums[pivot + 1] = nums[pivot + 1], nums[pivot]
-            else:
-                i = len(nums) - 1
-                half = pivot + int((i-pivot)/2)
-                changed = False
-                while i > half:
-                    if not changed and nums[pivot] < nums[i]:
-                        nums[pivot], nums[i] = nums[i], nums[pivot]
-                        changed = True
-                    #reverse list
-                    opposite = pivot + len(nums) - i
-                    nums[i], nums[opposite] = nums[opposite], nums[i]
-                    i -= 1
-                if not changed:
-                    i = half + 1
-                    while i < len(nums):
-                        if nums[pivot] < nums[i]:
-                            nums[pivot], nums[i] = nums[i], nums[pivot]
+            if nums[i] < nums[i + 1]:
+                # reverse array after pivot
+                j = len(nums) - 1
+                k = i + 1
+                while j > k:
+                    nums[j], nums[k] = nums[k], nums[j]
+                    j -= 1
+                    k += 1
+                # find replacement
+                lo = i + 1
+                hi = len(nums) - 1
+                while lo <= hi:
+                    mid = lo + ((hi - lo) >> 1)
+                    if nums[mid] > nums[i]:
+                        if nums[i] >= nums[mid - 1]:
+                            nums[mid], nums[i] = nums[i], nums[mid]
                             break
-                        i += 1
+                        else:
+                            hi = mid - 1
+                    else:
+                        lo = mid + 1
+                break
+            i -= 1
         else:
             nums.reverse()
 
-def test(nums):
-   print('%s --> ' % ','.join(map(str, nums)), end="")
-   Solution().nextPermutation(nums)
-   print(','.join(map(str, nums)))
+
+def test(nums, exps):
+    print('%s --> ' % ','.join(map(str, nums)), end="")
+    Solution().nextPermutation(nums)
+    if nums == exps:
+        print(','.join(map(str, nums)))
+    else:
+        print('%s (FAILED!!!)' % ','.join(map(str, nums)))
 
 if __name__ == '__main__':
-    test([3, 2, 1])
-    test([1, 2, 3])
-    test([1, 1, 5])
-    test([4,2,1,3])
-    test([1, 3, 2])
-    test([5, 5, 5])
-    test([1, 3, 2, 0])
-    test([5,4,7,5,3,2])
-    test([2, 3, 1])
-    test([2,2,0,4,3,1])
-    test([2,1,2,2,2,2,2,1])
+    test([3, 2, 1], [1, 2, 3])
+    test([1, 2, 3], [1, 3, 2])
+    test([1, 1, 5], [1, 5, 1])
+    test([4, 2, 1, 3], [4, 2, 3, 1])
+    test([1, 3, 2], [2, 1, 3])
+    test([5, 5, 5], [5, 5, 5])
+    test([1, 3, 2, 0], [2, 0, 1, 3])
+    test([5, 4, 7, 5, 3, 2], [5, 5, 2, 3, 4, 7])
+    test([2, 3, 1], [3, 1, 2])
+    test([2, 2, 0, 4, 3, 1], [2, 2, 1, 0, 3, 4])
+    test([2, 1, 2, 2, 2, 2, 2, 1], [2, 2, 1, 1, 2, 2, 2, 2])
